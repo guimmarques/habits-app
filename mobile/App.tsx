ce08f2ce56e740f7f1b3e5504778466734207f1f
Guilherme Marques <gmoraesm12@gmail.com>
2023-05-01 19:25:30 -0300

    second lesson

---
+++ mobile/App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from "@expo-google-fonts/inter";
+import { Home } from "./src/screens/Home";
+import { Loading } from "./src/components/Loading";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <Home />
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+    </>
+  );
+}
